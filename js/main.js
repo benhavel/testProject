ef54f4b4cb7245d3a2dbb10f3f4668564222d9a8
Benjamin L. Havel <Benjamin@Bens-MacBook-Pro-2.local>
2019-04-11 03:51:07 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -16,3 +16,52 @@
  
           el.addEventListener('click', onTabClick, false);
         })();
+
+
+
+
+var images = [
+
+  {
+    path:"./img/photo5.png",
+
+    desc: "photo of the city"
+  },
+  {
+    path:"./img/photo4.png",
+
+    desc: "picture of a girl"
+  },
+  {
+    path:"./img/photo3.png",
+
+    desc: "picture of a girl"
+  }
+
+];
+
+var image = document.getElementById("inc");
+
+var counter = 0;
+
+function changeImage() {
+  counter ++;
+
+  if(counter >= images.length) {
+    counter = 0;
+  }
+  image.src = images[counter].path;
+  image.alt = images[counter].desc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
